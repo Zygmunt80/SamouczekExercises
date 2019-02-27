738613dae7bde2f0d10b1fdbb27f7dc682ea1e6c
miki <miki@192.168.0.171>
2019-02-27 14:50:28 +0100

    done exercise 4

---
+++ SamouczekExercises/src/part5LoopsAndStatements/Exercise4.java
@@ -0,0 +1,15 @@
+package part5LoopsAndStatements;
+
+public class Exercise4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int counter = -10;
+		while (counter < 40) {
+			if (counter % 2 != 0) {
+				System.out.println(counter);
+			}
+			counter++;
+		}
+	}
+}
