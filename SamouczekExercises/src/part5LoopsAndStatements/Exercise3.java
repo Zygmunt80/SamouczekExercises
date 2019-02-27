e6c4f0d1c5a470775311af8694911c7fde2226be
miki <miki@192.168.0.171>
2019-02-27 14:46:48 +0100

    done 3rd exercise

---
+++ SamouczekExercises/src/part5LoopsAndStatements/Exercise3.java
@@ -0,0 +1,13 @@
+package part5LoopsAndStatements;
+
+public class Exercise3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = -10; i < 41; i++) {
+			if (i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+	}
+}
