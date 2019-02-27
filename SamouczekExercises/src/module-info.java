83a41d545d5beb861251224883fcd3796e6e6d0d
miki <miki@192.168.0.171>
2019-02-27 14:24:04 +0100

    created first empty file

--- SamouczekExercises/src/module-info.java
+++ SamouczekExercises/src/module-info.java
@@ -2,8 +2,10 @@
  * 
  */
 /**
- * @author miki
+ * @author Marcin Zygmunt
  *
+ *
+ *it will be package with exercises to tasks from site samouczekprogramisty.pl
  */
 module SamouczekExercises {
 }
---
+++ SamouczekExercises/src/part5LoopsAndStatements/Exercise1.java
@@ -0,0 +1,10 @@
+package part5LoopsAndStatements;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("co jest");
+	}
+
+}
