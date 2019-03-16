bead5b4f1a1323608d8aa39f03ba4b1f234f920d
miki <miki@192.168.0.171>
2019-03-16 11:59:53 +0100

    improved exercise with pairs (map of pairs)

--- SamouczekExercises/src/collections/Pairs.java
+++ SamouczekExercises/src/collections/Pairs.java
@@ -12,38 +12,33 @@ public class Pairs {
 	public Scanner scan = new Scanner(System.in);
 	public HashMap<String, String> pairs = new HashMap<>();
 	public static boolean end = false;
-	
+
 	public String userInput() {
 		return scan.nextLine();
 	}
-	
+
 	public void printPairs() {
 		for (String key : pairs.keySet()) {
 			String value = pairs.get(key);
 			System.out.println(key + " : " + value);
 		}
 	}
-	
+
 	public void addName(String name1, String name2) {
 		pairs.put(name1, name2);
 	}
-	
+
 	public void general() {
 		System.out.println("podaj pierwsze imiê pary");
 		String name1 = userInput();
-		if (name1.equals("-")) {
+		System.out.println("podaj drugiê imiê pary");
+		String name2 = userInput();
+		if (name1.equals("-") || name2.equals("-")) {
 			printPairs();
 		} else {
-			System.out.println("podaj drugiê imiê pary");
-			String name2 = userInput();
 			addName(name1, name2);
-			if (name2.equals("-")) {
-				printPairs();
-			} else {
-				general();
-			}
+			general();
 		}
-		
 	}
 
 	public static void main(String[] args) {
